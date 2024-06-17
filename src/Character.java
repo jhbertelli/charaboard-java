72655091e164e2e3e0662eca9b70d4b253f264b6
Marisa Kirisame <e>
2024-06-17 19:50:14 -0300

    undo gender to string change

--- src/Character.java
+++ src/Character.java
@@ -5,11 +5,11 @@ public class Character implements IFavoritable<CharacterFavorite> {
     private int age;
     private String description;
     private String race;
-    private String gender;
+    private Gender gender;
     private ArrayList<Game> relatedGames = new ArrayList<>();
     private ArrayList<CharacterFavorite> favorites = new ArrayList<>();
 
-    public Character(String name, int age, String description, String race, String gender) {
+    public Character(String name, int age, String description, String race, Gender gender) {
         this.name = name;
         this.age = age;
         this.description = description;
@@ -56,11 +56,11 @@ public class Character implements IFavoritable<CharacterFavorite> {
     public void setRace(String race) {
         this.race = race;
     }
-    public String getGender() {
+    public Gender getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
+    public void setGender(Gender gender) {
         this.gender = gender;
     }
 
--- src/FakeCharacters.java
+++ src/FakeCharacters.java
@@ -2,66 +2,74 @@ public class FakeCharacters {
     public static Character[] getFakeCharacters() {
 
         Character flandreScarlet = new Character(
-                "Flandre Scarlet",
-                495,
-                "Aluna da School of Magic, tabém uma antiga viajante do cruzeiro Summer.\nPossui olhos escarlate, cabelo loiro curto preso em um único lado por um rabo de cavalo.\nSua principal característica é ser uma vampira com grande vontade de destruição",
-                "Vampiro",
-                "Feminino");
+            "Flandre Scarlet",
+            495,
+            "Aluna da School of Magic, tabém uma antiga viajante do cruzeiro Summer.\nPossui olhos escarlate, cabelo loiro curto preso em um único lado por um rabo de cavalo.\nSua principal característica é ser uma vampira com grande vontade de destruição",
+            "Vampiro",
+            Gender.Female);
 
         Character solidSnake = new Character(
-                "Solid Snake",
-                52,
-                "Seu nome real é David, mas também é conhecido como Old Snake, Iroquois Pliskin ou simplesmente Snake.\nNasceu em 1972, possui nacionalidade norte americana (Estados Unidos).\nFoi criado como um clone do soldado Big Boss, inicialmente participou dos boina verdes e posteriormente introduzido na Unidade de Forças Especiais de Alta Tecnologia FOXHOUND na década de 1990",
-                "Humano",
-                "Masculino");
+            "Solid Snake",
+            52,
+            "Seu nome real é David, mas também é conhecido como Old Snake, Iroquois Pliskin ou simplesmente Snake.\nNasceu em 1972, possui nacionalidade norte americana (Estados Unidos).\nFoi criado como um clone do soldado Big Boss, inicialmente participou dos boina verdes e posteriormente introduzido na Unidade de Forças Especiais de Alta Tecnologia FOXHOUND na década de 1990",
+            "Humano",
+            Gender.Male);
 
         Character kratos = new Character(
-                "Kratos",
-                200,
-                "Kratos, também conhecido como o Fantasma de Esparta é o protagonis de mais de 8 jogos da série God of War, onde atua como uma espécie de anti-herói tentando mudar seu destino, ao mesmo tempo que trucida praticamente, ou se não absolutamente, todos os deuses do olimpo",
-                "Deus",
-                "Homem");
+            "Kratos",
+            200,
+            "Kratos, também conhecido como o Fantasma de Esparta é o protagonis de mais de 8 jogos da série God of War, onde atua como uma espécie de anti-herói tentando mudar seu destino, ao mesmo tempo que trucida praticamente, ou se não absolutamente, todos os deuses do olimpo",
+            "Deus",
+            Gender.Male);
 
         Character redPokemon = new Character(
-                "Red",
-                20,
-                "Red (Japonês: レッド Red) é o personagem com quem o jogador joga na geração 1 de jogos, Pokémon red, blue, green, yellow e é a escolha de personagem masculino em nos remakes da terceira geração, Pokémon FireRed e LeafGreen",
-                "Humano",
-                "Homem");
+            "Red",
+            20,
+            "Red (Japonês: レッド Red) é o personagem com quem o jogador joga na geração 1 de jogos, Pokémon red, blue, green, yellow e é a escolha de personagem masculino em nos remakes da terceira geração, Pokémon FireRed e LeafGreen",
+            "Humano",
+            Gender.Male);
 
-        Character laraCroft = new Character("Lara Croft",
-                21,
-                "Lara é a protagonista de série Tomb Raider, é uma arqueóloga britânica que se aventura em antigas tumbas e ruínas perigosas à procura de artefatos valiosos",
-                "Humano",
-                "Mulher");
+        Character laraCroft = new Character(
+            "Lara Croft",
+            21,
+            "Lara é a protagonista de série Tomb Raider, é uma arqueóloga britânica que se aventura em antigas tumbas e ruínas perigosas à procura de artefatos valiosos",
+            "Humano",
+            Gender.Female);
+
         //esses próximos eu pedi pro gpt pra testar
         Character character1 = new Character(
-                "Liora Sunblade",
-                27,
-                "Liora é uma guerreira habilidosa com uma espada encantada que brilha com uma luz dourada. Ela é conhecida por sua bravura e senso de justiça, sempre defendendo os fracos e oprimidos.",
-                "Elfo",
-                "Feminino"
+            "Liora Sunblade",
+            27,
+            "Liora é uma guerreira habilidosa com uma espada encantada que brilha com uma luz dourada. Ela é conhecida por sua bravura e senso de justiça, sempre defendendo os fracos e oprimidos.",
+            "Elfo",
+            Gender.Female
         );
 
         Character character2 = new Character(
-                "Thorin Oakenshield",
-                150,
-                "Thorin é um anão robusto e determinado, com uma barba longa e trançada. Ele é um líder nato, determinado a recuperar o reino perdido de sua linhagem e proteger seu povo a qualquer custo.",
-                "Anão",
-                "Masculino"
+            "Thorin Oakenshield",
+            150,
+            "Thorin é um anão robusto e determinado, com uma barba longa e trançada. Ele é um líder nato, determinado a recuperar o reino perdido de sua linhagem e proteger seu povo a qualquer custo.",
+            "Anão",
+            Gender.Male
         );
 
         Character character3 = new Character(
-                "Elysia Nightshade",
-                34,
-                "Elysia é uma feiticeira misteriosa com olhos violetas e cabelo prateado. Ela possui vasto conhecimento sobre magia antiga e é frequentemente consultada por aqueles que procuram sabedoria e poder.",
-                "Humano",
-                "Feminino"
+            "Elysia Nightshade",
+            34,
+            "Elysia é uma feiticeira misteriosa com olhos violetas e cabelo prateado. Ela possui vasto conhecimento sobre magia antiga e é frequentemente consultada por aqueles que procuram sabedoria e poder.",
+            "Humano",
+            Gender.Female
         );
 
-
         return new Character[] {
-                flandreScarlet,kratos,laraCroft,redPokemon,character3,character1,character2,solidSnake
+            flandreScarlet,
+            kratos,
+            laraCroft,
+            redPokemon,
+            character1,
+            character2,
+            character3,
+            solidSnake
         };
     }
 }
